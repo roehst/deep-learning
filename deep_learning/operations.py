54b1f26c24f9d9b723357d5b576d0088353927ed
Rodrigo Stevaux <rodrigo.stevaux@loft.com.br>
2020-09-29 19:14:51 -0300

    Simplify module

--- deep_learning/operations.py
+++ deep_learning/operations.py
@@ -1,7 +1,44 @@
-from abc import abstractmethod
-from . import Operation
+from abc import ABC, abstractmethod
+
 from numpy import ndarray
 import numpy as np
+
+
+class Operation(ABC):
+    def forward(self, input: ndarray) -> ndarray:
+        assert not np.any(np.isnan(input))
+        self._input = input
+        self._output = self.output()
+        return self._output
+
+    def backward(self, output_grad: ndarray) -> ndarray:
+        self._input_grad = self.input_grad(output_grad)
+        return self._input_grad
+
+    @abstractmethod
+    def output(self) -> ndarray:
+        ...
+
+    @abstractmethod
+    def input_grad(self, output_grad: ndarray) -> ndarray:
+        ...
+
+
+class Linear(Operation):
+    def output(self) -> ndarray:
+        return self._input
+
+    def input_grad(self, output_grad: ndarray) -> ndarray:
+        return output_grad
+
+
+class Sigmoid(Operation):
+    def output(self) -> ndarray:
+        return 1.0 / (1.0 + np.exp(-1.0 * self._input))
+
+    def input_grad(self, output_grad: ndarray) -> ndarray:
+        sigmoid_backward = self._output * (1.0 - self._output)
+        return sigmoid_backward * output_grad
 
 
 class ParamOperation(Operation):
--- deep_learning/operations/__init__.py
+++
@@ -1,6 +0,0 @@
-from deep_learning.operations.operation import Operation, Sigmoid, Linear
-from deep_learning.operations.param_operation import (
-    ParamOperation,
-    WeightMultiply,
-    BiasAdd,
-)
